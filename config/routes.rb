1e69f9f1c9d95a63dd5af5db1699918b43f3ad4a
takahashi0212 <rt6965@yahoo.co.jp>
2020-08-17 11:43:45 +0900

    既読機能のサーバーサイド実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'posts', to: 'posts#index'
+  #get 'posts', to: 'posts#index'
   #get 'posts/new', to: 'posts#new'
+  root to: 'posts#index' #localhost:3000/に特定のページを表示させたい（ルートへのルーティング設定）
   post 'posts', to: 'posts#create'
-  root :to => 'posts#index' #localhost:3000/に特定のページを表示させたい（ルートへのルーティング設定）
+  get 'posts/:id', to: 'posts#checked'
 end
