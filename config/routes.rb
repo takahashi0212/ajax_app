1a93776589208e2109d45100b211740dbebdddec
takahashi0212 <rt6965@yahoo.co.jp>
2020-08-17 11:02:10 +0900

    AjaxAppの準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'posts', to: 'posts#index'
-  get 'posts/new', to: 'posts#new'
+  #get 'posts/new', to: 'posts#new'
   post 'posts', to: 'posts#create'
+  root :to => 'posts#index' #localhost:3000/に特定のページを表示させたい（ルートへのルーティング設定）
 end
